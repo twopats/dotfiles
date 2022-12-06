2171d55010c96578e0d909d974cafb35d819e9ed
twopats <lpatipat@icloud.com>
2022-12-06 08:45:57 -0500

    fix: read script syntax

--- read.sh
+++ read.sh
@@ -4,7 +4,7 @@
 #!/bin/bash
 
 SYNC_DIR=~/dotfiles/
-echo "reaeing config files with ${SYNC_DIR}"
+echo "reading config files with ${SYNC_DIR}"
 # targets=(~/utils, ~/.config/{i3,lvim,nvim,kitty,lvim})
 # for target in "$targets"
 # do
@@ -13,4 +13,4 @@ echo "reaeing config files with ${SYNC_DIR}"
 # done
 cp $SYNC_DIR*.sh ~/code/utils/ 
 cp $SYNC_DIR{i3,kitty,nvim,lvim} ~/.config/ -r
-cp $SYNC_DIR.gitconfig,.bashrc,.bash_aliases,.tmux.conf} ~/
+cp $SYNC_DIR{.gitconfig,.bashrc,.bash_aliases,.tmux.conf} ~/
